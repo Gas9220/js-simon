3c10cfd58861e0ae9e6b7283f59ab88fa5a127cf
Gaspare <Gas9220@users.noreply.github.com>
2023-03-06 18:59:57 +0100

    Esercizio completo di UI

--- main.js
+++ main.js
@@ -17,36 +17,89 @@ function generateRandomNumber(numbersToGenerate) {
         }
     } while (numbers.length < numbersToGenerate)
 
-    console.log(numbers);
+    console.log("Random Numbers", numbers);
     return numbers;
 }
 
-function collectUserChoices(numbersToCollect) {
+function collectUserChoices(answerElement) {
     let userChoices = [];
 
-    while (userChoices.length < numbersToCollect) {
-        const userChoice = Number(prompt(`Qual'era il numero in ${userChoices.length + 1} posizione`));
-        userChoices.push(userChoice);
-    }
+    answerElement.forEach(answer => {
+        userChoices.push(Number(answer.value))
+    });
 
-    console.log(userChoices);
+    console.log("User choice", userChoices);
     return userChoices;
 }
 
-const randomNumbers = generateRandomNumber(5);
-const userChoices = collectUserChoices(5);
-
-function checkChoices(randomNumbers, userChoices) {
+function checkChoices(randomNumbers, answerElement) {
+    let userChoices = collectUserChoices(answerElement);
     let correctAnswers = [];
 
     for (let index = 0; index < randomNumbers.length; index++) {
         if (randomNumbers[index] === userChoices[index]) {
             correctAnswers.push(userChoices[index]);
-        }       
+            answerElement[index].classList.add('correct-answer');
+        } else {
+            answerElement[index].classList.add('wrong-answer');
+        }
     }
+
+    setUI()
 
     console.log(correctAnswers)
     return correctAnswers;
 }
 
-const result = checkChoices(randomNumbers, userChoices);
+function hideUI() {
+    cardElements.forEach(card => {
+        card.innerHTML = "*"
+    });
+
+    answerElement.forEach(answer => {
+        answer.classList.remove('hidden');
+    });
+}
+
+function updateUI() {
+    counterText.innerText = `Memorizza questi numeri in ${counter} secondi`;
+
+    if (counter > 0) {
+        counter--
+    } else {
+        clearInterval(displayTimer);
+        counterText.innerText = `Indovina i numeri`;
+    }
+}
+
+function setUI() {
+    for (let index = 0; index < cardElements.length; index++) {
+        cardElements[index].innerText = randomNumbers[index]    
+    }
+}
+
+//const result = checkChoices(randomNumbers, userChoices);
+
+const counterElement = document.getElementById('counter');
+const cardElements = document.querySelectorAll('.number');
+const counterText = document.getElementById('counter-text');
+const answerElement = document.querySelectorAll('.answer');
+const confirmBtn = document.getElementById('confirm-btn');
+
+console.log(cardElements)
+
+let counter = 10
+
+const randomNumbers = generateRandomNumber(5);
+setUI()
+
+const displayTimer = setInterval(updateUI, 1000)
+
+const timer = setTimeout(hideUI, counter * 1000);
+
+
+confirmBtn.addEventListener('click',
+    function () {
+        checkChoices(randomNumbers, answerElement)
+    }
+)
