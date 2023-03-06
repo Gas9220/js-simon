b0c2291716bd5ee885b7cbef68830aaab4233935
Gaspare <Gas9220@users.noreply.github.com>
2023-03-06 17:28:06 +0100

    Inserito l'input numeri e il controllo risposte

--- main.js
+++ main.js
@@ -17,8 +17,36 @@ function generateRandomNumber(numbersToGenerate) {
         }
     } while (numbers.length < numbersToGenerate)
 
-    console.log(numbers)
+    console.log(numbers);
     return numbers;
 }
 
-generateRandomNumber(5);
+function collectUserChoices(numbersToCollect) {
+    let userChoices = [];
+
+    while (userChoices.length < numbersToCollect) {
+        const userChoice = Number(prompt(`Qual'era il numero in ${userChoices.length + 1} posizione`));
+        userChoices.push(userChoice);
+    }
+
+    console.log(userChoices);
+    return userChoices;
+}
+
+const randomNumbers = generateRandomNumber(5);
+const userChoices = collectUserChoices(5);
+
+function checkChoices(randomNumbers, userChoices) {
+    let correctAnswers = [];
+
+    for (let index = 0; index < randomNumbers.length; index++) {
+        if (randomNumbers[index] === userChoices[index]) {
+            correctAnswers.push(userChoices[index]);
+        }       
+    }
+
+    console.log(correctAnswers)
+    return correctAnswers;
+}
+
+const result = checkChoices(randomNumbers, userChoices);
